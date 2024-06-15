6779cbeceb06ebbddecb90602599709326e202ad
Riya <21cs74@ecajmer.ac.in>
2024-06-15 15:19:12 +0530

    hhhh

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -8,7 +8,7 @@ function Form(){
     const [city , setCity] = useState("");
     const [condition , setCondition] = useState("False")
     let d ={};
-    const url ="https://weatherapp-backend-a5ry.onrender.com/city";
+    const url ="https://weatherapp-backend-a5ry.onrender.com/city" || "http://localhost:5000/city";
     const [weatherData , setWeatherData] = useState({
         city:"",
         temp:"",
